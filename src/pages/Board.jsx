660b9baea23803d68b4ea1d2d73a5627ce5d39f6
SaiCharanUvaraj <saicharanuvi01@gmail.com>
2024-08-28 20:21:09 +0530

    home raw content

--- src/pages/Board.jsx
+++ src/pages/Board.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const Board = () => {
+  list=["Sai Charan","Aldous Roy","Koushik Babu","Viknesh SR"]
   return (
     <div>
         <p className='text-3xl font-bold pt-20 text-center'>Board Content</p>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
-
 const Home = () => {
   return (
-    <div>
+    <div className='grid place-items-center'>
         <p className='text-3xl font-bold pt-20 text-center'>Home Content</p>
+        <p className='text-3xl font-bold pt-20 text-center'> Welcome to LeetRank: Where Code Meets Competition!</p>
+        <p className='text-3xl font-bold pt-20 text-center'>Track Your Progress, Rank Among the Best!</p>
+        <button className='text-3xl font-bold pt-20 text-center'></button>
     </div>
   )
 }
